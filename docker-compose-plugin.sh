78a044929e9409074e7044abe791200384070c7f
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-06-14 15:37:07 +0530

    Create docker-compose-plugin.sh

---
+++ docker-compose-plugin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt install docker.io -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
